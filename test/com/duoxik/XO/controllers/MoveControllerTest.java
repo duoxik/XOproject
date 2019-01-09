1d66818b4f16bb28c18d4595ee7f0f46f62fc2b2
duoxik <duoxik@gmail.com>
2019-01-09 23:43:01 +0300

    MoveControllerTest created

---
+++ test/com/duoxik/XO/controllers/MoveControllerTest.java
@@ -0,0 +1,30 @@
+package com.duoxik.XO.controllers;
+
+import com.duoxik.XO.model.Exceptions.AlreadyOccupiedException;
+import com.duoxik.XO.model.Exceptions.InvalidPointException;
+import com.duoxik.XO.model.Field;
+import com.duoxik.XO.model.Figure;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class MoveControllerTest {
+
+    @Test
+    public void applyFigure() throws AlreadyOccupiedException, InvalidPointException {
+
+        final Field inputField = new Field();
+        final Point inputPoint = new Point(1, 1);
+        final Figure inputFigure = Figure.X;
+
+        MoveController moveController = new MoveController();
+        moveController.applyFigure(inputField, inputFigure, inputPoint);
+
+        try {
+            moveController.applyFigure(inputField, inputFigure, inputPoint);
+            fail();
+        } catch (final AlreadyOccupiedException e) {}
+    }
+}
