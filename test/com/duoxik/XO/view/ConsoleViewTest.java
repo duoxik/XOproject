da81a7b458353d17f6670a6504ff8f0b721f7754
duoxik <duoxik@gmail.com>
2019-01-10 17:23:01 +0300

    ConsoleViewTest created

--- test/com/duoxik/XO/view/ConsoleViewTest.java
+++ test/com/duoxik/XO/view/ConsoleViewTest.java
@@ -3,6 +3,7 @@ package com.duoxik.XO.view;
 import com.duoxik.XO.model.Exceptions.InvalidPointException;
 import com.duoxik.XO.model.Field;
 import com.duoxik.XO.model.Figure;
+import com.duoxik.XO.model.Game;
 import org.junit.Test;
 
 import java.awt.*;
@@ -12,12 +13,16 @@ public class ConsoleViewTest {
     @Test
     public void show() throws InvalidPointException {
 
+
         final Field field = new Field(4);
         field.setFigure(new Point(1, 1), Figure.X);
         field.setFigure(new Point(2, 1), Figure.O);
-        ConsoleView consoleView = new ConsoleView(field);
 
-        consoleView.show();
+        final Game game = new Game(null, field, null);
+
+        ConsoleView consoleView = new ConsoleView();
+
+        consoleView.show(game);
 
     }
 }
