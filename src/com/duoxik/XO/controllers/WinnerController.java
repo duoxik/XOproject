7c0ac027248a0b6814a4fae9652f385acf9eef45
duoxik <duoxik@gmail.com>
2019-01-10 15:02:27 +0300

    Fixes for WinnerController

--- src/com/duoxik/XO/controllers/WinnerController.java
+++ src/com/duoxik/XO/controllers/WinnerController.java
@@ -43,12 +43,14 @@ public class WinnerController {
 
         try {
             currentFigure = field.getFigure(currentPoint);
+            if (currentFigure == null) return false;
             nextFigure = field.getFigure(nextPoint);
+
         } catch (final InvalidPointException e) {
             return true;
         }
 
-        if (currentFigure == null) return false;
+
 
         if (currentFigure != nextFigure) return false;
 
