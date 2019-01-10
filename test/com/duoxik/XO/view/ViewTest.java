4740da818f1517c7ba756fceacf35f4bb8437884
duoxik <duoxik@gmail.com>
2019-01-10 13:42:17 +0300

    ViewTest created

---
+++ test/com/duoxik/XO/view/ViewTest.java
@@ -0,0 +1,25 @@
+package com.duoxik.XO.view;
+
+import com.duoxik.XO.model.Exceptions.InvalidPointException;
+import com.duoxik.XO.model.Field;
+import com.duoxik.XO.model.Figure;
+import org.junit.Test;
+
+import java.awt.*;
+
+import static org.junit.Assert.*;
+
+public class ViewTest {
+
+    @Test
+    public void showField() throws InvalidPointException {
+
+        Field field = new Field(4);
+        field.setFigure(new Point(1, 1), Figure.X);
+        field.setFigure(new Point(2, 1), Figure.O);
+        View view = new View(field);
+
+        view.showField();
+
+    }
+}
